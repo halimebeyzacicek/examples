f9590d41fc7f702ab313410af3f9e9b7ee6c7179
halimebeyzacicek <hbcicek25@gmail.com>
2021-01-24 01:01:45 +0300

    add form1-form2

---
+++ Formlar Arası Geçiş (VisualStudio)/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form1 frm1;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Form1 frm1 = new Form1();
+            //frm1.Show();
+            //Hide();
+
+            Form1 frm1 = (Form1)Application.OpenForms["Form1"];
+            Application.OpenForms["Form1"].Show();
+            Hide();
+        }
+    }
+}
