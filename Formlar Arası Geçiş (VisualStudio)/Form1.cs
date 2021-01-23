e357a1b5d4ea9f2ef21b9a5694632136da354ca4
halimebeyzacicek <hbcicek25@gmail.com>
2021-01-24 00:56:27 +0300

    .

---
+++ Formlar Arası Geçiş (VisualStudio)/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int kontrol=0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 frm2 = new Form2();
+            //frm2.Show();
+            //Hide();
+
+            //HATA
+            //Form2 frm2 = (Form2)Application.OpenForms["Form2"];
+            //Application.OpenForms["Form2"].Show();
+            //Hide();
+
+            if (kontrol == 0)
+            {
+                kontrol = 1;
+                Form2 frm2 = new Form2();
+                frm2.Show();
+                Hide();
+            }
+            else
+            {
+                Form2 frm2 = (Form2)Application.OpenForms["Form2"];
+                Application.OpenForms["Form2"].Show();
+                Hide();
+            }
+        }
+    }
+}
